73dbbc3b9cce0f53bbd8ecddf5b4ea165defe72f
KAZUHIRO JINMA <k-jinma@uhe.ac.jp>
2025-12-02 15:22:48 +0900

    1202途中

---
+++ src/lesson75/Program2.java
@@ -0,0 +1,23 @@
+package lesson75;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		
+		String fileName = "data.txt";
+		fileOpen(fileName);
+		
+	}
+	
+	public static void fileOpen(String fileName) {
+		
+		File file = new File(fileName);
+		
+		// FileNotFoundException発生の可能性あり
+		Scanner scanner = new Scanner(file); 
+		
+	}
+}
